13400cc174045958e80db3f101e8a712140382e0
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-08-07 03:06:45 +0530

    Added all the methods in users service and added create and delete routes in controller.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
+import { Body, Delete, Param, Post } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+	constructor(private usersService: UsersService) {}
+
+	@Post('/register')
+	createUser(@Body() body: CreateUserDto) {
+		return this.usersService.create(body);
+	}
+
+	@Delete('/:id')
+	removeUser(@Param('id') id: string) {
+		return this.usersService.remove(id);
+	}
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './user.schema';
+
+interface FindAttrs {
+	email: string;
+	username: string;
+}
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+	constructor(@InjectModel(User.name) private model: Model<UserDocument>) {}
+
+	create(createUserDto: CreateUserDto): Promise<User> {
+		const user = new this.model(createUserDto);
+
+		return user.save();
+	}
+
+	findOne(id: string) {
+		if (!id) return null;
+		return this.model.findById(id);
+	}
+
+	find(attrs: Partial<FindAttrs>) {
+		return this.model.find(attrs);
+	}
+
+	findByEmailOrUsername(email: string, username: string) {
+		return this.model.find({
+			$or:
+				[
+					{ email },
+					{ username }
+				]
+		});
+	}
+
+	async update(id: string, attrs: Partial<User>) {
+		const user = await this.findOne(id);
+		if (!user) {
+			throw new NotFoundException('user not found!');
+		}
+		Object.assign(user, attrs);
+		return user.save();
+	}
+
+	async remove(id: string) {
+		const user = await this.findOne(id);
+		if (!user) {
+			throw new NotFoundException('user not found!');
+		}
+		return user.remove();
+	}
+}
