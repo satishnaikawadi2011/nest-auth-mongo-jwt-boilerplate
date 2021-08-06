abd11404f3584b796dbe488723afebe427b169a8
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-08-07 02:11:12 +0530

    Added connection with mongodb atlas database.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports:
+		[
+			MongooseModule.forRoot(`mongodb+srv://satishnaikawadi:satish2001@cluster0.ebdzk.mongodb.net/nest-auth-mongo-jwt-boilerplate
+?retryWrites=true&w=majority`)
+		],
+	controllers:
+		[
+			AppController
+		],
+	providers:
+		[
+			AppService
+		]
 })
 export class AppModule {}
