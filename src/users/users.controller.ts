a844a2468e8782b3cb7ef314cf9f3ef74664e40c
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-08-07 11:45:00 +0530

    Completed all CRUD routes related to User.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Delete, Param, Post } from '@nestjs/common';
+import { Body, Delete, Get, NotFoundException, Param, Patch, Post } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -10,6 +11,20 @@ export class UsersController {
 	@Post('/register')
 	createUser(@Body() body: CreateUserDto) {
 		return this.usersService.create(body);
+	}
+
+	@Get('/:id')
+	async findUser(@Param('id') id: string) {
+		const user = await this.usersService.findOne(id);
+		if (!user) {
+			throw new NotFoundException('user not found !');
+		}
+		return user;
+	}
+
+	@Patch('/:id')
+	updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+		return this.usersService.update(id, body);
 	}
 
 	@Delete('/:id')
